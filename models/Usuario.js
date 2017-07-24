bf7c29eef6953bafd3ee99b166a4e0ece5015a5c
CaioDonizetti <caiodonizettiqueiroz@gmail.com>
2017-07-24 00:54:15 -0300

    Finalizando api e interface

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,8 +3,16 @@ var Schema = mongoose.Schema;
  
 var UsuarioSchema = new Schema({
     nome: String,
-    login: String,
-    senha: String
+    email: { type: String, unique: true, index: true },
+    senha: String,
+    cpf: { type: String, maxlength: 14, trim:true},
+    rg: { type: String, maxlength: 10, trim:true},
+    sexo: { type: String, maxlength: 1, trim:true, uppercase: true },
+    telefone: String,
+    dataNascimento: Date,
+    cidade: String,
+    cep: String,
+    estado: { type: String, trim:true},
 });
  
 module.exports = mongoose.model('Usuario', UsuarioSchema);
--- server.js
+++ server.js
@@ -1,41 +1,23 @@
-//Base do Setup da Aplicação:
-
-/* Chamada das Packages que iremos precisar para a nossa aplicação */
-var express = require('express'); //chamando o pacote express
-var app = express(); //definção da nossa aplicação através do express
-var bodyParser = require('body-parser');  //chamando o pacote body-parser
-
-/** Configuração da variável 'app' para usar o 'bodyParser()'.
- * Ao fazermos isso nos permitirá retornar os dados a partir de um POST
- */
+//Base de configurações e rotas da api:
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/** Definição da porta onde será executada a nossa aplicação */
-var port = process.env.PORT || 8000;
 
-//Rotas da nossa API:
+//Configurando criptografia para ser usada nas senhas
 //==============================================================
-
-/* Aqui o 'router' irá pegar as instâncias das Rotas do Express */
-var router = express.Router();
-
-/* Rota de Teste para sabermos se tudo está realmente funcionando (acessar através: GET: http://localhost:8000/api) */
-router.get('/', function (req, res) {
-    res.json({ message: 'YEAH! Seja Bem-Vindo a nossa API' });
-});
-
-/* TODO - Definir futuras rotas aqui!!! */
-
-/* Todas as nossas rotas serão prefixadas com '/api' */
-app.use('/api', router);
+const saltRounds = 10;
+var salt = bcrypt.genSaltSync(saltRounds);
 
 //Iniciando o Servidor (Aplicação):
 //==============================================================
+var port = process.env.PORT || 8000;
 app.listen(port);
-console.log('Iniciando a aplicação na porta ' + port);
 
-//Configuração Base da Aplicação:
+//Configuração base de dados da Aplicação:
 //====================================================================================
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://root:xexuto65@ds013366.mlab.com:13366/data');
@@ -46,46 +28,54 @@ var Usuario = require('./models/Usuario');
 
 
 
-//Rotas da nossa API:
+//Rotas da API:
 //==============================================================
 
-/* Aqui o 'router' irá pegar as instâncias das Rotas do Express */
+//Aqui irá pegar as instâncias das Rotas do Express 
 var router = express.Router();
 
-/* Middleware para usar em todos os requests enviados para a nossa API- Mensagem Padrão */
+//Interceptando requisições e adicionando cors
 router.use(function (req, res, next) {
-    console.log('Algo está acontecendo aqui........');
-    next(); //aqui é para sinalizar de que prosseguiremos para a próxima rota. E que não irá parar por aqui!!!
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-/* Rota de Teste para sabermos se tudo está realmente funcionando (acessar através: GET: http://localhost:8000/api) */
+//Todas as rotas serão prefixadas com '/api'
+app.use('/api', router);
+
 router.get('/', function (req, res) {
-    res.json({ message: 'YEAH! Seja Bem-Vindo a nossa API' });
+    res.json("Teste");
 });
 
-// Rotas que irão terminar em '/usuarios' - (servem tanto para: GET All &amp; POST)
 router.route('/usuarios')
-
-    /* 1) Método: Criar Usuario (acessar em: POST http://localhost:8080/api/usuarios */
     .post(function (req, res) {
         var usuario = new Usuario();
 
-        //aqui setamos os campos do usuario (que virá do request)
-        usuario.nome = req.body.nome;
-        usuario.login = req.body.login;
-        usuario.senha = req.body.senha;
+        usuario.nome            = req.body.nome;
+        usuario.email           = req.body.email;
+        usuario.senha           = bcrypt.hashSync(req.body.senha, salt);
+        usuario.rg              = req.body.rg;
+        usuario.sexo            = req.body.sexo;
+        usuario.telefone        = req.body.telefone;
+        usuario.dataNascimento  = req.body.dataNascimento;
+        usuario.cidade          = req.body.cidade;
+        usuario.cep             = req.body.cep;
+        usuario.estado          = req.body.estado;
 
         usuario.save(function (error) {
-            if (error)
-                res.send(error);
+            if (error){
+                if(error.code && error.code == "11000"){
+                    res.status(400).send("E-mail já cadastrado, por favor informe outro e-mail, ou tente recuperar sua senha!");    
+                }
+                res.status(400).send(error);
+            }
 
             res.json({ message: 'Usuário criado!' });
         });
     })
 
     .get(function (req, res) {
-
-        //Função para Selecionar Todos os 'usuarios' e verificar se há algum erro:
         Usuario.find(function (err, usuarios) {
             if (err)
                 res.send(err);
@@ -94,13 +84,8 @@ router.route('/usuarios')
         });
     });
 
-// Rotas que irão terminar em '/usuarios/:usuario_id' - (servem tanto para GET by Id, PUT, &amp; DELETE)
 router.route('/usuarios/:usuario_id')
-
-    /* 3) Método: Selecionar Por Id (acessar em: GET http://localhost:8080/api/usuarios/:usuario_id) */
     .get(function (req, res) {
-
-        //Função para Selecionar Por Id e verificar se há algum erro:
         Usuario.findById(req.params.usuario_id, function (error, usuario) {
             if (error)
                 res.send(error);
@@ -109,18 +94,21 @@ router.route('/usuarios/:usuario_id')
         });
     })
     .put(function (req, res) {
-
-        //Primeiro: Para atualizarmos, precisamos primeiro achar o Usuario. Para isso, vamos selecionar por id:
         Usuario.findById(req.params.usuario_id, function (error, usuario) {
             if (error)
                 res.send(error);
 
-            //Segundo: Diferente do Selecionar Por Id... a resposta será a atribuição do que encontramos na classe modelo:
-            usuario.nome = req.body.nome;
-            usuario.login = req.body.login;
-            usuario.senha = req.body.senha;
+            usuario.nome            = req.body.nome;
+            usuario.email           = req.body.email;
+            usuario.senha           = bcrypt.hashSync(req.body.senha, salt);
+            usuario.rg              = req.body.rg;
+            usuario.sexo            = req.body.sexo;
+            usuario.telefone        = req.body.sexo;
+            usuario.dataNascimento  = req.body.dataNascimento;
+            usuario.cidade          = req.body.cidade;
+            usuario.cep             = req.body.cep;
+            usuario.estado          = req.body.estado;
 
-            //Terceiro: Agora que já atualizamos os campos, precisamos salvar essa alteração....
             usuario.save(function (error) {
                 if (error)
                     res.send(error);
@@ -130,8 +118,6 @@ router.route('/usuarios/:usuario_id')
         });
     })
     .delete(function(req, res) {
- 
-        //Função para excluir os dados e também verificar se há algum erro no momento da exclusão:
         Usuario.remove({
         _id: req.params.usuario_id
         }, function(error) {
@@ -142,6 +128,15 @@ router.route('/usuarios/:usuario_id')
         });
     });
 
-
-/* Todas as nossas rotas serão prefixadas com '/api' */
-app.use('/api', router);
+router.route('/autenticar')
+    .post(function (req, res) {
+        var senhaCrip = bcrypt.hashSync(req.body.senha, salt);
+        Usuario.find({email: req.body.email, senha: senhaCrip}, function (error, usuario) {
+            if (error)
+                res.status(400).send(error);
+            if(usuario && usuario.length > 0){
+                res.json(usuario);
+            }
+            res.status(400).send("Email ou senha inválidos!");
+        });
+    });
